81b0387e81a44cc5a985f293f760c9530d7de2bf
keita <keitaf.0344@gmail.com>
2020-09-13 00:04:08 +0900

    微修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'Wander, Wonder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
