08287245bef83a14d80034e9ba49186095bccf2a
keita <keitaf.0344@gmail.com>
2020-09-13 22:26:56 +0900

    google search consoleの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'google-site-verification', name: 'google-site-verification', content: 'OgO9qwM2XY0qY5xaUMHmOlBtHCl6mI-bK052Sw_C60A'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon1.png' }
