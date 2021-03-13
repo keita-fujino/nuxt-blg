3eaf560503aeba20dfd4df165591633e36057be3
keita <keitaf.0344@gmail.com>
2021-03-13 13:40:28 +0900

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   head: {
     titleTemplate: '%s',
     // title: process.env.npm_package_name || '',
-    title: 'Fujinote',
+    title: 'Keta-log',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
